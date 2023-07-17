f0b0ba9fd5ef32c376f1699773408045494593b3
prog <prog@DESKTOP-AMG781M>
2023-07-17 19:54:54 -0300

    commit parcail

---
+++ VendasWebAPI/DbContextMysql/MySQLDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VendasWebAPI.Entities;
+
+namespace VendasWebAPI.DbContextMysql
+{
+    public class MySQLDbContext : DbContext
+    {
+        public MySQLDbContext(DbContextOptions<MySQLDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categoria> Categorias { get; set; }
+
+    }
+}
