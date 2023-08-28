266fabec7c20c4393191fa053a459e216f60352a
prog <prog@DESKTOP-AMG781M>
2023-08-28 18:37:50 -0300

    atualização

---
+++ VendasDesktop/Model/CadastrarCategoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendasDesktop.Model
+{
+    internal class CadastrarCategoria
+    {
+       
+        public string Nome { get; set; }
+
+        public int Medida { get; set; }
+
+        public string Tipo { get; set; }
+
+        
+    }
+}
