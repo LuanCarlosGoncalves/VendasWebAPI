c44846ca25cdd28fcc742767735ae736466191e0
lucas17943 <lucas17943@gmail.com>
2023-08-28 19:10:21 -0300

    comit parcial

--- VendasDesktop/CadCategoria.cs
+++ VendasDesktop/CadCategoria.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,19 +8,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VendasDesktop.Model;
 
 namespace VendasDesktop
 {
     public partial class CadCategoria : Form
     {
+        private readonly HttpClient _httpClient;
+
         public CadCategoria()
         {
             InitializeComponent();
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7234/");
         }
 
-        private void btnSalvar_Click(object sender, EventArgs e)
+        private async void btnSalvar_Click(object sender, EventArgs e)
         {
+            var cadastrarCategoria = new CadastrarCategoria();
 
+            cadastrarCategoria.Nome = txtNome.Text;
+            cadastrarCategoria.Medida = Convert.ToInt32(txtMedida.Text);
+            cadastrarCategoria.Tipo = txtTipo.Text;
+
+            var categoriaJson = JsonConvert.SerializeObject(cadastrarCategoria);
+            
+            var conteudo = new StringContent(categoriaJson, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _httpClient.PostAsync("api/Categoria", conteudo);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var resposta = await response.Content.ReadAsStringAsync();
+
+                MessageBox.Show(resposta);
+            }
+            else
+            {
+                var resposta = await response.Content.ReadAsStringAsync();
+                MessageBox.Show("ERRO --->"+resposta);
+
+            }
         }
     }
 }
